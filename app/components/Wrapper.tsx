db0fc6d28822a490b7122374009685e029558d87
Cecile-Hirschauer <cecile.hirschauer@bbox.fr>
2025-01-15 09:50:12 +0100

    UI: Create Wrapper component

---
+++ app/components/Wrapper.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type WrapperProps = {
+  children: React.ReactNode;
+};
+
+const Wrapper = ({ children }: WrapperProps) => {
+  return <div>{children}</div>;
+};
+
+export default Wrapper;
