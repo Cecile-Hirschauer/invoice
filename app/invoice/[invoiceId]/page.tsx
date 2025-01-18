751bf1f747597213491254fc1da37fbd33d4c44a
Cecile-Hirschauer <cecile.hirschauer@bbox.fr>
2025-01-18 17:29:27 +0100

    feat(display invoice details): display header of invoice detail with the status and 2 buttons: save and delete

--- app/invoice/[invoiceId]/page.tsx
+++ app/invoice/[invoiceId]/page.tsx
@@ -1,5 +1,66 @@
-const page = ({ params }: { params: Promise<{ invoiceId: string }> }) => {
-  return <div></div>;
+"use client";
+import { getInvoiceById } from "@/app/actions";
+import Wrapper from "@/app/components/Wrapper";
+import { Invoice } from "@/types";
+import { Save, Trash } from "lucide-react";
+import { useEffect, useState } from "react";
+
+const InvoicePage = ({
+  params,
+}: {
+  params: Promise<{ invoiceId: string }>;
+}) => {
+  const [invoice, setInvoice] = useState<Invoice | null>(null);
+  const [initialInvoice, setInitialInvoice] = useState<Invoice | null>(null);
+
+  const fetchInvoice = async () => {
+    try {
+      const { invoiceId } = await params;
+      const fetchedInvoice = await getInvoiceById(invoiceId);
+
+      if (fetchedInvoice) {
+        setInvoice(fetchedInvoice);
+        setInitialInvoice(fetchedInvoice);
+      }
+    } catch (error) {
+      console.error(`Erreur lors de la récupération de la facture: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    fetchInvoice();
+  });
+  return (
+    <Wrapper>
+      <div>
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center mb-4">
+          <p className="badge badge-ghost badge-lg uppercase">
+            <span>Facture-</span> {invoice?.id}
+          </p>
+          <div className="flex mt-4 md:mt-0">
+            <select
+              value={invoice?.status}
+              className="select select-sm select-bordered w-full"
+            >
+              <option value={1}>Brouillon</option>
+              <option value={2}>En attente</option>
+              <option value={3}>Payée</option>
+              <option value={4}>Annulée</option>
+              <option value={5}>Impayée</option>
+            </select>
+            <button className="btn btn-sm ml-4 btn-secondary">
+              Sauvegarder
+              <Save className="w-4 h-4 ml-2" />
+            </button>
+            <button className="btn btn-sm ml-4 btn-accent">
+              <Trash className="w-4 h-4" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </Wrapper>
+  );
 };
 
-export default page;
+export default InvoicePage;
